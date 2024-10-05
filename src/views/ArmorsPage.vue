77ea5c89fa646b27ad974fc2ebb4eae01a1a0d7e
Ragnar <47790420+ragnar05@users.noreply.github.com>
2024-10-05 19:28:16 +0100

    Update Armors 2
    Update Armors 2

--- src/views/ArmorsPage.vue
+++ src/views/ArmorsPage.vue
@@ -38,8 +38,7 @@
     <!-- Sección de Ofertas -->
     <section class="mt-12 bg-gray-200 p-4 md:p-8 rounded-lg">
       <h2 class="text-3xl font-bold text-gray-800 mb-6 text-center">
-        Special Offers
-      </h2>
+        Armors
       <div class="grid grid-cols-1 gap-8">
         <!-- Contenedor del ProductCard -->
         <div class="bg-white rounded-lg shadow-md overflow-hidden w-full">
@@ -53,7 +52,6 @@
             ]"
             :images="[
               '/img/Armors/White_King1.jpg',
-              '/img/Armors/White_King1.jpg',
             ]"
             :urlIngameVideo="'https://www.youtube.com/watch?v=6v2L2UGZJAM'"
           />
@@ -62,7 +60,7 @@
         <!-- ESTE DIV ES EL QUE SE COPIA-->
         <div class="bg-white rounded-lg shadow-md overflow-hidden w-full">
           <ProductCard
-            :productTitle="'Ejemplo'"
+            :productTitle="'King Monkey'"
             :features="[
               'Costume (+hairstyle, icons, msm)',
               'Weapons',
@@ -70,8 +68,39 @@
               'Effects',
             ]"
             :images="[
-              '/img/Armors/White_King1.jpg',
-              '/img/Armors/White_King1.jpg',
+              '/img/Armors/King_Monkey2.jpg',
+            ]"
+            :urlIngameVideo="'https://www.youtube.com/watch?v=6v2L2UGZJAM'"
+          />
+        </div>
+
+        <div class="bg-white rounded-lg shadow-md overflow-hidden w-full">
+          <ProductCard
+            :productTitle="'Red Bull'"
+            :features="[
+              'Costume (+hairstyle, icons, msm)',
+              'Weapons',
+              'Wings',
+              'Effects',
+            ]"
+            :images="[
+              '/img/Armors/Red_Bull3.jpg',
+            ]"
+            :urlIngameVideo="'https://www.youtube.com/watch?v=6v2L2UGZJAM'"
+          />
+        </div>
+
+        <div class="bg-white rounded-lg shadow-md overflow-hidden w-full">
+          <ProductCard
+            :productTitle="'Blood Moon'"
+            :features="[
+              'Costume (+hairstyle, icons, msm)',
+              'Weapons',
+              'Wings',
+              'Effects',
+            ]"
+            :images="[
+              '/img/Armors/Blood_Moon4.jpg',
             ]"
             :urlIngameVideo="'https://www.youtube.com/watch?v=6v2L2UGZJAM'"
           />

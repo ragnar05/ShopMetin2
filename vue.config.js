dea0f2487bed303171f4755a887e886613140d07
Edvin Freyer Ortega <edvintrabajo@gmail.com>
2024-10-09 20:59:13 +0100

    Añadido al vue.config.js configuración para github pages

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/ShopMetin2/" : "/",
   configureWebpack: {
     output: {
       libraryExport: "default",
@@ -9,4 +11,7 @@ module.exports = defineConfig({
   css: {
     extract: false, // Esto incrustará el CSS en los archivos JavaScript
   },
+  outputDir: "dist", // El directorio que será servido por GitHub Pages
+  assetsDir: "static", // Opcionalmente, para organizar los assets
+  productionSourceMap: false, // Para evitar los source maps en producción
 });
